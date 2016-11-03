5cd4b6e94773464f4c3fb14c2b9f4a0d83833033
shotat <shotat@users.noreply.github.com>
2016-11-03 12:45:43 +0900

    Initial commit

---
+++ lib/slash_force.rb
@@ -0,0 +1,12 @@
+require "slash_force/version"
+
+module SlashForce
+  def append_trailing_slash
+    url, q, parameters = request.original_url.partition('?')
+    redirect_to "#{url}/#{q}#{parameters}" unless trailing_slash_present?(url)
+  end
+
+  def trailing_slash_present?(str)
+    str.to_s.last == '/'
+  end
+end
---
+++ lib/slash_force/version.rb
@@ -0,0 +1,3 @@
+module SlashForce
+  VERSION = "0.1.0"
+end
