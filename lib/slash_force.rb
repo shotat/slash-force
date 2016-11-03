9cda0122bbe4409cacd07d47bcd98f0305a4bb45
shotat <shotat@users.noreply.github.com>
2016-11-03 14:10:12 +0900

    ver0.2

--- lib/slash_force.rb
+++ lib/slash_force.rb
@@ -6,6 +6,8 @@ module SlashForce
     redirect_to "#{url}/#{q}#{parameters}" unless trailing_slash_present?(url)
   end
 
+  private
+
   def trailing_slash_present?(str)
     str.to_s.last == '/'
   end
